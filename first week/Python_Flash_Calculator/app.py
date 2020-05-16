b9305f748b5a9f30f26fb628ef1bba490286c966
Nicolas Rementeria <nicolas.rementeria@gmail.com>
2020-05-16 19:57:08 -0300

    Full simple calculator

--- first week/Python_Flash_Calculator/app.py
+++ first week/Python_Flash_Calculator/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,5 +9,24 @@ def main():
 
 @app.route('/send', methods=['POST'])
 
-def send():
+def send(sum=sum):
+    if request.method == 'POST':
+        
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
     
+        if operation == 'add':
+            sum = float(num1) + float(num2)
+            return render_template('app.html', sum=sum)
+        elif operation == 'substract':
+            sum = float(num1) - float(num2)
+            return render_template('app.html', sum=sum)
+        elif operation == 'multiply':
+            sum = float(num1) * float(num2)
+            return render_template('app.html', sum=sum)
+        elif operation == 'divide':
+            sum = float(num1) / float(num2)
+            return render_template('app.html', sum=sum)
+        else:
+            return render_template('app.html')
